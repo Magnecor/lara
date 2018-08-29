1e9e0b8a7758e1f800a6ef8971bd3a7415919697
Magnecor <magnecora@gmail.com>
2018-08-29 20:55:46 +0300

    Добавление маршрутов изучаем дальше ларавел

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.layouts.app_admin')
+
+<!-- загрузка осноного шаблона из папки laravel/resources/views/admin/layouts фаил app_admin.blade.php  layouts - папка и app - это фаил  app_admin.blade.php  -->
+
+@section('content') <!-- // в секцию 'content' в файле app_admin.blade.php -->
+
+<h1>Админ зашел на сайт</h1>
+
+@endsection
